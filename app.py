c685ae925020845b2af3946d659e705d1ca85552
markwilde33 <markwilde29@gmail.com>
2018-10-09 20:32:13 +0100

    Added a small dictionary called posts

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dr Hannibal Lecter',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 30, 2018'
+    },
+    {
+        'author': 'Clarice Starling',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 1, 2018'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, Clarice, KISS MY FACE!'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
