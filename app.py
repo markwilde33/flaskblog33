926c3440cc820e10b221b2c97440dae2f1c05826
markwilde33 <markwilde29@gmail.com>
2018-10-09 16:40:08 +0100

    Uninstalled requests

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def about():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=(os.environ.get('PORT')),
             debug=True)
