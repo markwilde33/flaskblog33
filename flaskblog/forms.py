f2302ff8e676ef375b9d314163e6eb623ff0ba90
markwilde33 <markwilde29@gmail.com>
2018-10-12 21:17:16 +0100

    added a form to the account page, users can add a unique photo and update their account details

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -33,3 +34,23 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update This Bitch')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username taken, please choose again.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email taken, please choose again.')
