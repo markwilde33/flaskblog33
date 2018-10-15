299c35fafc34837dbe1dd2bab24229bc817b334a
markwilde33 <markwilde29@gmail.com>
2018-10-15 14:26:35 +0100

    Created a route, function, form and page for new posts from users. Added link to navbar

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -4,7 +4,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -114,5 +114,14 @@ def account():
                            image_file=image_file, form=form)
 
 
-# '/Users/markwilde/Flask_Blog/flaskblog/static/profile_pics/picture_fn'
-# /Users/markwilde/Flask_Blog/flaskblog
+@app.route('/post/new')
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post just went live motherfucker')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+    # '/Users/markwilde/Flask_Blog/flaskblog/static/profile_pics/picture_fn'
+    # /Users/markwilde/Flask_Blog/flaskblog
